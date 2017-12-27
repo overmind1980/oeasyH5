8e008edd58aed7e81210a02dc543e9c4c1582dc3
overmind <overmind1980@gmail.com>
2017-12-27 20:51:54 +0800

    modify

---
+++ 002Console/js/alert.js
@@ -0,0 +1,18 @@
+function woDeTaoLu(){
+    alert("你猜一会儿会发生什么");
+    alert("再等等 ");
+    alert("也许真有奇迹");
+}
+
+alert("你怎么打开了这个网页");
+
+woDeTaoLu();
+alert("你相信奇迹吗？");
+alert("让我来向你提个问题");
+woDeTaoLu();
+
+alert("果然没有奇迹");
+alert("我们就这样放弃吗？");
+woDeTaoLu();
+
+alert("这是最后一句了，88");
